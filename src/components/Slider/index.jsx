46c090a6f23a29706918c8a6cb0c676ddf47bc91
felipeFerreiraffl <felipado2x0@gmail.com>
2025-01-11 12:46:01 -0300

    feat: new slider

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,9 +1,64 @@
-import { SliderArea } from "../../styles/components/slider";
+import { useRef, useState } from "react";
+import { HiArrowCircleLeft, HiArrowCircleRight } from "react-icons/hi";
+import "swiper/css";
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import test from "../../assets/images/img-test.png";
+import colors from "../../styles/colors/colors";
+import {
+  Image,
+  SliderArea,
+  SliderButton,
+} from "../../styles/components/slider";
+
+const images = [test, test, test, test, test, test, test, test, test, test];
 
 export default function SliderContent() {
+  const [slidesPerView, setSlidesPerView] = useState(4);
+  const [showPrevButton, setShowPrevButton] = useState(false);
+
+  const prevButtonRef = useRef(null);
+  const nextButtonRef = useRef(null);
+
   return (
     <SliderArea>
-      
+      {showPrevButton && (
+        <SliderButton ref={prevButtonRef.current} className="prev-button">
+          <HiArrowCircleLeft size={81} color={colors.main.pk600} />
+        </SliderButton>
+      )}
+
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation={{
+          nextEl: nextButtonRef.current,
+          prevEl: prevButtonRef.current,
+        }}
+        pagination={{ clickable: true }}
+        slidesPerView={slidesPerView}
+        spaceBetween={30}
+        onSlideChange={(swiper) => {
+          setShowPrevButton(swiper.activeIndex > 1);
+        }}
+        onSwiper={(swiper) => {
+          swiper.params.navigation.prevEl = prevButtonRef.current;
+          swiper.params.navigation.nextEl = nextButtonRef.current;
+          swiper.navigation.update();
+        }}
+      >
+        {images.map((img, i) => (
+          <SwiperSlide key={i}>
+            <div>
+              <Image src={img} alt="Imagem" />
+              <p>{i}º</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <SliderButton ref={nextButtonRef.current} className="next-button">
+        <HiArrowCircleRight size={81} color={colors.main.pk600} />
+      </SliderButton>
     </SliderArea>
   );
 }
--- src/styles/components/slider.js
+++ src/styles/components/slider.js
@@ -2,6 +2,28 @@ import styled from "styled-components";
 
 export const SliderArea = styled.div`
     margin-top: 1.75rem; // 18px
+    position: relative;
+    display: flex;
+    align-items: center;
+    gap: 1.875rem; // 30px
+    width: 100%;
+
+    .swiper-container {
+        width: 100%;
+        height: 100%;
+    }
+
+    .next-button,
+    .prev-button {
+        z-index: 10;
+        transition: 0.3s ease-in-out;
+    }
+
+    .next-button:hover,
+    .prev-button:hover {
+        filter: brightness(1.2);
+        transform: scale(1.1);
+    }
 `;
 
 export const Image = styled.img`
@@ -9,4 +31,10 @@ export const Image = styled.img`
     height: 282px;
     /* aspect-ratio: 67 / 94; */
     border-radius: 5px;
+`;
+
+export const SliderButton = styled.button`
+    background: none;
+    border: none;
+    cursor: pointer;
 `;
