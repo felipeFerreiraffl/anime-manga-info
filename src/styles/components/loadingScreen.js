2a86eb2102d06b8e8c7d9e789800ba27f9a7b50d
felipeFerreiraffl <felipado2x0@gmail.com>
2025-01-20 12:00:10 -0300

    docs: new responsive loading screen

--- src/styles/components/loadingScreen.js
+++ src/styles/components/loadingScreen.js
@@ -10,14 +10,26 @@ export const LoadingContainer = styled.div`
   height: 100vh;
   gap: 0.5rem;
   background: ${colors.gradient.ppGrd};
+
+  @media (max-width: 430px) {
+   gap: 1rem; // 16px
+  }
 `;
 
 export const LoadingLogo = styled.img`
   width: 10rem;
   aspect-ratio: 1 / 1;
+
+  @media (max-width: 430px) {
+    width: 8.125rem; // 130px 
+  }
 `;
 
 export const LoadingText = styled.p`
   font: ${fonts.detail.dtl32};
   color: ${colors.neutral.nt0};
+
+  @media (max-width: 430px) {
+    font: ${fonts.detail.dtl16};
+  }
 `;
